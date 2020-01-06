12e69e5b6688686c423c2087f65011f399a528a5
Kyler Middleton <kymidd@gmail.com>
2020-01-05 21:49:23 -0700

    init repo

---
+++ terraform/envA/main.tf
@@ -0,0 +1,41 @@
+# Require TF version to most recent
+terraform {
+  required_version = "~> 0.12.18"
+}
+
+# Download any stable version in AWS provider of 2.27.0 or higher
+provider "aws" {
+  version = "~> 2.27.0"
+  region  = "us-east-1"
+}
+
+# Build the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.1.0.0/16"
+  instance_tenancy     = "default"
+
+  tags = {
+    Name      = "Vpc"
+    Terraform = "true"
+  }
+}
+
+# Build route table 1
+resource "aws_route_table" "route_table1" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "RouteTable1"
+    Terraform = "true"
+  }
+}
+
+# Build route table 2
+resource "aws_route_table" "route_table2" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "RouteTable2"
+    Terraform = "true"
+  }
+}
